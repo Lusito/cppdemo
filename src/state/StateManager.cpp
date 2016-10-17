294b62c59dd897f9ef88d3c07f7b2ae5173927c0
Lusito <core@roughael.net>
2016-10-17 15:18:40 +0200

    ensure state will not be deleted during its own callback call

--- src/state/StateManager.cpp
+++ src/state/StateManager.cpp
@@ -6,8 +6,10 @@ StateManager::StateManager() { }
 StateManager::~StateManager() { }
 
 void StateManager::push(std::shared_ptr<AbstractState> state) {
-	if(!states.empty())
-		states.back()->obscuring();
+	if(!states.empty()) {
+		auto back = states.back();
+		back->obscuring();
+	}
 
 	states.push_back(state);
 	state->entered();
@@ -18,7 +20,10 @@ bool StateManager::pop() {
 	if(states.empty())
 		return false;
 	
-	states.back()->leaving();
+	{
+		auto back = states.back();
+		back->leaving();
+	}
 	states.pop_back();
 	if(!states.empty()) {
 		auto back = states.back();
@@ -29,19 +34,25 @@ bool StateManager::pop() {
 }
 
 void StateManager::update(float deltaTime) {
-	if(!states.empty())
-		states.back()->update(deltaTime);
+	if(!states.empty()) {
+		auto back = states.back();
+		back->update(deltaTime);
+	}
 }
 
 void StateManager::handleKey(int key, int scancode, int action, int mods) {
-	if(!states.empty())
-		states.back()->handleKey(key, scancode, action, mods);
+	if(!states.empty()) {
+		auto back = states.back();
+		back->handleKey(key, scancode, action, mods);
+	}
 }
 
 void StateManager::resize(int width, int height) {
 	this->width = width;
 	this->height = height;
 	
-	if(!states.empty())
-		states.back()->resize(width, height);
+	if(!states.empty()) {
+		auto back = states.back();
+		back->resize(width, height);
+	}
 }
